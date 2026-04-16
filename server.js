c1aeac2a7bef247dbbb77b2434505d5ff5935ba2
Jonatas6199 <44883581+Jonatas6199@users.noreply.github.com>
2026-04-15 21:09:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -210,7 +210,11 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // ==========================
 // SERVIDOR
 // ==========================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
